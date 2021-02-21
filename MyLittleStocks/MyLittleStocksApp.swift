8f153dfbcb1209d936f0aa644b08c4546a05d0c5
Тимур Чеберда <cheberda@tochkak.ru>
2021-02-21 23:03:59 +0300

    Initial Commit

---
+++ MyLittleStocks/MyLittleStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLittleStocksApp.swift
+//  MyLittleStocks
+//
+//  Created by Тимур Чеберда on 21.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyLittleStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
